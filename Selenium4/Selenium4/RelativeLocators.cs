c0e94aad3a5441e6873217e62f655d2cf009253a
csv-hezrontan <84384687+csv-hezrontan@users.noreply.github.com>
2021-11-03 13:26:58 +0800

    Added relative locators test

---
+++ Selenium4/Selenium4/RelativeLocators.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Selenium4
+{
+    [TestClass]
+    public class RelativeLocators
+    {
+        public ChromeDriver Driver { get; set; }
+
+        [TestInitialize]
+        public void Init()
+        {
+            Driver = new ChromeDriver();
+        }
+
+        [TestMethod]
+        public void RelativeLocatorTest()
+        {
+            Driver.Manage().Window.Maximize();
+            Driver.Navigate().GoToUrl("https://magenicautomation.azurewebsites.net/Static/Training3/loginpage.html");
+
+
+            IWebElement usernameField = Driver.FindElement(By.Id("name"));
+            IWebElement passwordField = Driver.FindElement(By.Id("pw"));
+            IWebElement loginButton = Driver.FindElement(RelativeBy.WithLocator(By.TagName("button")).Below(passwordField));
+            
+            usernameField.SendKeys("Ted");
+            passwordField.SendKeys("123");
+            loginButton.Click();
+        }
+
+        [TestCleanup]
+        public void CleanUp()
+        {
+            Driver.Dispose();
+        }
+    }
+}
